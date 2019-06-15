641fc9e919633de16423e0e90b1bf380197a9b99
Kevin Simper <kevin.simper@gmail.com>
2019-06-15 23:30:17 +0200

    Update to v2 and add date

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const http = require('http')
 const PORT = process.env.PORT || 9000
 http.createServer((req, res) => {
   console.log('New connection')
-  res.end('Hello Cloud Run')
+  res.end('Hello Cloud Run v2 ' + new Date() )
 }).listen(PORT, () => console.log('Listening on', PORT))
